7893de5be42605750de377eb59af3ac703dd6a9a
Ignacio Falk <flakolefluk@gmail.com>
2023-11-11 11:05:33 -0300

    remove state type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,4 @@
-import { PluginOption } from 'vite';
 declare global {
-	function $state<T>(value: T): T;
 	function $states<T extends any[]>(...values: T): T
 
 	function $log(...args: any[]): void;
