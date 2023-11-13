4a28cf3ed48af9f46193fe950a137ba62f03e196
Ignacio Falk <flakolefluk@gmail.com>
2023-11-13 09:26:49 -0300

    add wrap example

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,9 +3,13 @@ declare global {
 
 	function $log(...args: any[]): void;
 
-	function $gsp<G>(getObj: G): Readonly<G>
-	function $gsp<G, S>(getObj: G, setObj: S): Omit<Readonly<G>, keyof S> & S
-	function $gsp<G, S, P>(getObj: G, setObj: S, propObj: P): Omit<Readonly<G>, keyof S | keyof P> & Omit<S, keyof P> & P
+	function $gsp<Getters>(getObj: Getters): Readonly<Getters>
+	function $gsp<Getters, Setters>(getObj: Getters, setObj: Setters): Omit<Readonly<Getters>, keyof Setters> & Setters
+	function $gsp<Getters, Setters, Properties>(getObj: Getters, setObj: Setters, propObj: Properties): Omit<Readonly<Getters>, keyof Setters | keyof Properties> & Omit<Setters, keyof Properties> & Properties
+
+	function $wrap<Writables>(writablesObj: Writables): Writables
+	function $wrap<Writables, Readables>(writablesObj: Writables, readablesObj: Readables): Omit<Readonly<Readables>, keyof Writables> & Writables
+	function $wrap<Writables, Readables, Properties>(writablesObj: Writables, readablesObj: Readables, propObj: Properties): Omit<Readonly<Readables>, keyof Writables | keyof Properties> & Omit<Writables, keyof Properties> & Properties
 
 	function $get<T>(getObj: T): Readonly<T>;
 	function $set<T>(setObj: T): T;
